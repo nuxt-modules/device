3ea5f94cf4d6c60f242475e16f3c5c44b38277ba
mvrlin <mvrlin@pm.me>
2019-02-20 13:51:54 +0300

    feat: add types to Context

--- types.d.ts
+++ types.d.ts
@@ -8,6 +8,16 @@ interface Device {
   isTablet: boolean
 }
 
+declare module '@nuxt/vue-app' {
+  interface Context {
+    isDesktop: boolean
+    isIos: boolean
+    isMobile: boolean
+    isMobileOrTablet: boolean
+    isTablet: boolean
+  }
+}
+
 declare module 'vue/types/vue' {
   interface Vue {
     $device: Device
