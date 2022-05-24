2e3224657975b3a60fd6c4deb7f34750fd312d27
Redemption <25185835+Redemption198@users.noreply.github.com>
2022-05-24 09:57:32 +0200

    Added Nuxt Bridge Compatibility
    Also added docs to the module config parameters

--- src/module.ts
+++ src/module.ts
@@ -4,15 +4,36 @@ import { defu } from 'defu'
 import { defineNuxtModule, addPlugin } from '@nuxt/kit'
 
 export interface ModuleOptions {
+  /**
+   * Enable Device Module
+   * @default true
+   * @type boolean
+   */
   enabled: boolean
+
+  /**
+   * Device Module Default User Agent
+   * @default 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36'
+   * @type string
+   */
   defaultUserAgent: string,
+
+  /**
+   * Refresh Device Module Values On Window Resize
+   * @default false
+   * @type boolean
+   */
   refreshOnResize: boolean
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'device-module',
-    configKey: 'device'
+    configKey: 'device',
+    compatibility: {
+      nuxt: '^3.0.0 || ^2.16.0',
+      bridge: true
+    }
   },
   defaults: {
     enabled: true,
