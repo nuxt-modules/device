01d736ed8dafb34fcdb8edee2cc2330ae4e15418
Redemption198 <25185835+Redemption198@users.noreply.github.com>
2022-07-16 00:11:55 +0200

    (chore): prepack the module

---
+++ dist/runtime/generateFlags.d.ts
@@ -0,0 +1,2 @@
+import type { Device } from './types';
+export default function generateFlags(headers: any, userAgent: any): Device;
--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,10 +1,9 @@
 
-import { ModuleOptions, ModuleHooks } from './module'
+import { ModuleOptions } from './module'
 
 declare module '@nuxt/schema' {
   interface NuxtConfig { ['device']?: Partial<ModuleOptions> }
   interface NuxtOptions { ['device']?: ModuleOptions }
-  interface NuxtHooks extends ModuleHooks {}
 }
 
 
