3108fe4441d50464530f67df988b03a8ecb89470
Redemption198 <25185835+Redemption198@users.noreply.github.com>
2022-10-17 11:38:40 +0200

    Restore bot-regex script

---
+++ scripts/create-bots-regex.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+const agentsJsonUrl = 'https://raw.githubusercontent.com/monperrus/crawler-user-agents/master/crawler-user-agents.json'
+async function main () {
+  const response = await fetch(agentsJsonUrl)
+  const items = await response.json()
+  const patterns = items.map(item => item.pattern)
+  const regex = patterns.join('|')
+  console.log(`/${regex}/`)
+}
+
+main()
