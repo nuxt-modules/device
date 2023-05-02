24a5b55ca508f60d324d7e66c8cf592f0efaaca5
Shinji Yamada <dotneet@gmail.com>
2023-05-03 00:23:11 +0900

    fix tests

--- test/module.test.ts
+++ test/module.test.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils-edge'
+import { setup, $fetch } from '@nuxt/test-utils'
 
 function findTrueOrFalse (objectToParse, input) {
   return objectToParse[objectToParse.indexOf(input) + input.length] === '✅'
