6d860a4b8d77e2a99ee6b95c679f28e0b62d3898
Damian Glowala <damian.glowala.rebkow@gmail.com>
2024-08-28 20:38:21 +0200

    chore: remove commented out module config from test fixture

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -2,9 +2,4 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/device',
   ],
-//   device: {
-  // enabled: true,
-  // defaultUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36',
-  // refreshOnResize: false
-//   },
 })
