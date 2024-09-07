54836638e1a3e4b4e468df1e3b6951143616c48a
Damian Glowala <damian.glowala.rebkow@gmail.com>
2024-09-07 12:27:45 +0200

    chore: update README and module options JSDoc

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,19 @@
 export interface ModuleOptions {
   /**
-   * Device Module Default User Agent
+   * Sets the default value for the `user-agent` header (useful when running `npm run generate`).
    * @default 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36'
    */
   defaultUserAgent?: string
 
   /**
-   * Whether to enable the module conditionally.
+   * Enables the module conditionally.
    * @default true
    * @deprecated
    */
   enabled?: boolean
 
   /**
-   * Refresh Device Module Values On Window Resize
+   * Refreshes flags on window resize.
    * @default false
    * @deprecated
    */
